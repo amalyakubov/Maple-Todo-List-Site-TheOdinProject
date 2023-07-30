0caadbedb8a910fe3dc104ca1cbaf1699385f385
amalyakubov <amal.yakubov7777@gmail.com>
2023-07-30 21:02:36 +0200

    Add task section to the page and stylize it

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { millisecondsToHours } from 'date-fns';
 import './style.css';
 import { getDisplayDate} from './time.js';
+import { loadToday } from './today.js';
+
+loadToday();
 
 console.log(getDisplayDate());
---
+++ src/today.js
@@ -0,0 +1,17 @@
+import { getDisplayDate } from "./time.js";
+
+export function loadToday() {
+    const CONTAINER = document.getElementById('main');
+    let date = getDisplayDate();
+    
+    const HEADER = document.createElement('p');
+    const HEADERCONTAINER = document.createElement('div');
+    HEADER.innerHTML = 'Today,';
+    const HEADERDATE = document.createElement('p');
+    HEADERDATE.id = 'headerdate';
+    HEADERDATE.innerHTML = `${date}`;
+    HEADERCONTAINER.appendChild(HEADER);
+    HEADERCONTAINER.appendChild(HEADERDATE);
+    HEADERCONTAINER.id = 'header';
+    CONTAINER.appendChild(HEADERCONTAINER);
+}
