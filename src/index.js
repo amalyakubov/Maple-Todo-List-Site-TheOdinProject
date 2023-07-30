98fc764adab760c004d52364afbf58202575625c
amalyakubov <amal.yakubov7777@gmail.com>
2023-07-30 22:28:55 +0200

    Add a way to save  tasks and create an inupt box for it

--- src/index.js
+++ src/index.js
@@ -4,5 +4,3 @@ import { getDisplayDate} from './time.js';
 import { loadToday } from './today.js';
 
 loadToday();
-
-console.log(getDisplayDate());
--- src/today.js
+++ src/today.js
@@ -1,4 +1,7 @@
 import { getDisplayDate } from "./time.js";
+import { taskFactory } from "./item-constructor.js";
+
+let TASKARRAY = [];
 
 export function loadToday() {
     const CONTAINER = document.getElementById('main');
@@ -14,4 +17,29 @@ export function loadToday() {
     HEADERCONTAINER.appendChild(HEADERDATE);
     HEADERCONTAINER.id = 'header';
     CONTAINER.appendChild(HEADERCONTAINER);
+
+    loadTaskCreator();
+    addInputListener();
+}
+
+function loadTaskCreator() {
+    const CONTAINER = document.getElementById('main');
+    let inputBox = document.createElement('input');
+    inputBox.type = 'text';
+    inputBox.id = 'taskInput';
+    inputBox.name = 'taskInput';
+    inputBox.placeholder = 'Add a task';
+    CONTAINER.appendChild(inputBox);
+}
+
+function addInputListener() { 
+    let dummy = document.getElementById('taskInput');
+    dummy.addEventListener('keypress', function (e) {
+        if (e.key === 'Enter' && document.hasFocus()) {
+            let task = new taskFactory(dummy.value);
+            TASKARRAY.push(task);
+            console.log(TASKARRAY);
+        }
+    })
+    
 }
