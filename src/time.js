d9d0cad5fc8cb305b76a02558ed73c93691cae35
amalyakubov <amal.yakubov7777@gmail.com>
2023-07-31 15:13:24 +0200

    Fix grammar of the months and add May to the list

--- src/time.js
+++ src/time.js
@@ -1,6 +1,6 @@
 export function getDisplayDate() {
-    const DAYNAMES = ['Monday', 'Tuseday', 'Wendesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    const MONTHS = ['January', 'February', 'March', 'April', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    const DAYNAMES = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     let date = new Date();
     let day = DAYNAMES[date.getDay()];
     let dayNumber = date.getDate();
