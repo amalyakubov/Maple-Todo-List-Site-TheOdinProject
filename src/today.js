bc0a6920e8f34821ca34b719222960930ca147df
amalyakubov <amal.yakubov7777@gmail.com>
2023-08-02 21:53:19 +0200

    Create the widgets for detailed task display
    Add an event listner to the task div that calls a function, which loads
    up the widget
    
    Add a placeholder for the project name to the widget
    Add an exit button to the widget
    Add the static, for now, dropdown menu to select the priority of the
    task

--- src/today.js
+++ src/today.js
@@ -77,12 +77,20 @@ function updateTaskDisplay() {
     for (const taskObject in array) {
         let task = document.createElement('div');
         let titleElement = document.createElement('p');
+
+        if (!task.classList.contains('hasevl')) {
+            task.addEventListener('click', () => {
+                loadWidget(task);
+            })
+            task.classList.add('hasevel');
+        }
+
         titleElement.id = 'title-element';
         titleElement.innerHTML = array[taskObject].title;
         let button = document.createElement('button');
         button.classList.add('do');
 
-        if (!button.classList.contains('hasevl')) {
+        if (!task.classList.contains('hasevl')) {
             button.addEventListener('click', () => {
                 let taskObject = JSON.parse(localStorage.getItem(titleElement.textContent));
                 completeUncompleteTask(taskObject);
@@ -109,4 +117,89 @@ function resetInput () {
     let input = document.getElementById('taskInput');
     input.value = 'Add a task';
     document.activeElement.blur();
+}
+
+function loadWidget(task) {
+    const WIDGET = document.createElement('div');
+    WIDGET.id = 'widget';
+
+    const NAV = document.createElement('div');
+    NAV.id = 'widget-nav';
+    const PROJECT = document.createElement('p');
+    PROJECT.textContent = 'Project : X';
+    NAV.appendChild(PROJECT);
+
+    const exitButton = document.createElement('button');
+    exitButton.textContent = 'Exit';
+
+    exitButton.addEventListener('click', () => {
+        WIDGET.remove();
+        
+    })
+    NAV.appendChild(exitButton);
+
+    WIDGET.appendChild(NAV);
+
+    const HEADER = document.createElement('div');
+    HEADER.id = 'widget-header';
+
+    let title = document.createElement('p');
+    title.textContent = task.firstChild.textContent;
+
+    HEADER.appendChild(createTaskButton());
+    HEADER.appendChild(title);
+    WIDGET.appendChild(HEADER);
+
+    const LIST = document.createElement('div');
+
+    const DATE = document.createElement('p');
+    DATE.textContent = 'dummy';
+    LIST.appendChild(DATE);
+
+    const PRIORITYCONTAINER = document.createElement('div');
+    const LABEL = document.createElement('label');
+    LABEL.for = 'priortiy';
+    LABEL.textContent = 'Priority';
+    const SELECT = document.createElement('select');
+    SELECT.name = 'priorities';
+    SELECT.id = 'priority';
+    
+    let optionOne = document.createElement('option');
+    optionOne.value = 1;
+    optionOne.textContent = 'One';
+    SELECT.appendChild(optionOne);
+    
+    let optionTwo = document.createElement('option');
+    optionTwo.value = 2;
+    optionTwo.textContent = 'Two';
+    SELECT.appendChild(optionTwo);
+
+    let optionThree = document.createElement('option');
+    optionThree.value = 3;
+    optionThree.textContent = 'Three';
+    SELECT.appendChild(optionThree);
+
+    let optionFour = document.createElement('option');
+    optionFour.value = 4;
+    optionFour.textContent = 'Four';
+    SELECT.appendChild(optionFour);
+    PRIORITYCONTAINER.appendChild(SELECT);
+    LIST.appendChild(PRIORITYCONTAINER);
+
+    WIDGET.appendChild(LIST);
+    document.body.appendChild(WIDGET);
+
+
+}
+
+function createTaskButton (task) {
+    let button = document.createElement('button');
+    button.addEventListener('click', () => {
+        let taskObject = JSON.parse(localStorage.getItem(titleElement.textContent));
+        completeUncompleteTask(taskObject);
+        button.classList.add('hasevl');
+        task.classList.remove('done');
+        updateTaskDisplay();
+    })
+    return button;
 }
